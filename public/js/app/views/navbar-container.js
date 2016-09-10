21891f62aa154ac902088223e8d1fe737ecc50b1
Bogdan Hryhorian <carerakjan@gmail.com>
2016-09-10 22:32:16 +0300

    updated navbar

--- public/js/app/views/navbar-container.js
+++ public/js/app/views/navbar-container.js
@@ -1,32 +1,84 @@
 define([
     'jquery',
     'underscore',
+    'backbone',
+    'socket',
     'common/base.view',
     'text!templates/navbar-container.html'
-], function($, _, BaseView, navBarContainerTemplate){
+], function($, _, Backbone, socket, BaseView, navBarContainerTemplate){
     return BaseView.extend({
 
         el: $("#navbar-container"),
 
+        model: new (Backbone.Model.extend({
+            defaults: {
+                useAutomation: true,
+                activePlatform: 'all'
+            }
+        }))(),
+
         events: {
             'submit #formRunBs' : 'submitForm',
-            'click #runBs' : 'submitForm'
+            'click #runBs' : 'submitForm',
+            'click #platform .dropdown-menu li a' : 'changePlatform',
+            'change #useAutomation': 'toggleUsingAutomation'
         },
 
         _templateString: navBarContainerTemplate,
 
         initialize: function(){
             BaseView.prototype.initialize.apply(this, arguments);
+            this.listenTo(this.options.app, 'app:updateSuites', this.updateSuites.bind(this));
             this.render();
         },
 
-        render: function() {
-            this.$el.html(this.template({}));
+        updateSuites: function(suites) {
+            this.model.set('suites', suites);
         },
 
-        submitForm: function() {}
+        render: function() {
+            this.$el.html(this.template({
+                platforms:  ['all', 'mobile', 'tablet', 'desktop']
+            }));
+        },
 
+        isValidUrl: function(url) {
+            try {
+                var re = /^\s*(((ht|f)tp(s?))\:\/\/)?(www.|[a-zA-Z].)[a-zA-Z0-9\-\.]+\.([a-z]{2,6})(\:[0-9]+)*(\/($|[a-zA-Z0-9\.\,\;\?\'\\\+&amp;%\$#\=~_\-]+))*\s*$/;
+                if(!url) throw Error('URL is required');
+                if(!re.test(url)) throw Error('URL validation error');
+                return true;
+            } catch (e) {
+                return alert(e.message);
+            }
+        },
 
+        submitForm: function(event) {
+            event.preventDefault();
+            this.options.app.trigger('app:eraseReports');
+            if(this.isValidUrl(this.$('#urlInput').val())) {
+                this.model.set('url', this.$('#urlInput').val());
+                socket.emit('run_bs', this.model.toJSON());
+            }
+        },
+
+        changePlatform: function(event) {
+            event.preventDefault();
+            this.resetDropDown();
+            this.$(event.target).closest('li').addClass('active');
+            this.model.set('activePlatform', $(event.target).html());
+            this.$('[data-toggle="dropdown"] .value').html(this.model.get('activePlatform'));
+        },
+
+        resetDropDown: function() {
+            this.$('#platform .dropdown-menu li').removeClass('active');
+        },
+
+        toggleUsingAutomation: function(event) {
+            event.preventDefault();
+            this.model.set('useAutomation', event.target.checked);
+            this.options.app.trigger('app:useAutomation', event.target.checked);
+        }
 
     });
 });
